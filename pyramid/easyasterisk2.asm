4f8c758fa73ff9c70a14fd7d087b22b4da128855
isaacdvid1596 <isaacdvid15.96@gmail.com>
2020-07-30 19:04:39 -0600

    Add files via upload

---
+++ pyramid/easyasterisk2.asm
@@ -0,0 +1,95 @@
+li $v0,11
+li $a0,32
+syscall
+li $v0,11
+li $a0,32
+syscall
+li $v0,11
+li $a0,32
+syscall
+
+li $v0,11
+li $a0,42
+syscall
+
+li $v0,11
+li $a0,10
+syscall
+
+li $v0,11
+li $a0,32
+syscall
+li $v0,11
+li $a0,32
+syscall
+
+li $v0,11
+li $a0,42
+syscall
+
+li $v0,11
+li $a0,32
+syscall
+
+li $v0,11
+li $a0,42
+syscall
+
+li $v0,11
+li $a0,10
+syscall
+
+li $v0,11
+li $a0,32
+syscall
+
+li $v0,11
+li $a0,42
+syscall
+
+li $v0,11
+li $a0,32
+syscall
+li $v0,11
+li $a0,32
+syscall
+li $v0,11
+li $a0,32
+syscall
+
+li $v0,11
+li $a0,42
+syscall
+
+li $v0,11
+li $a0,10
+syscall
+
+li $v0,11
+li $a0,42
+syscall
+li $v0,11
+li $a0,42
+syscall
+li $v0,11
+li $a0,42
+syscall
+li $v0,11
+li $a0,42
+syscall
+li $v0,11
+li $a0,42
+syscall
+li $v0,11
+li $a0,42
+syscall
+li $v0,11
+li $a0,42
+syscall
+
+li $v0,11
+li $a0,10
+syscall
+
+
+
